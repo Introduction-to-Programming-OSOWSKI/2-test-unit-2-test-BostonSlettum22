f6f28cd8370c6569197fd6701279720a68bc2736
BostonSlettum22 <816661@mystma.org>
2021-01-15 16:49:45 +0000

    test 1

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-#WRITE YOUR CODE IN THIS FILE
+def waterState(f):
+    if f < 30:
+        return "solid"
+    elif f < 80:
+        return "liquid"
+    else:
+        return "gas"
+    
+print(waterState(412))
+
+def isDozen(d):
+    if d % 12 == 0:
+        return True
+    else:
+        return False
+
+print(isDozen(60))
+
+def toGerman(x):
+    if x == "yes":
+        return "ja"
+    else:
+        return "nein"
+
+print(toGerman("yes"))
+
+def stopLight(x):
+    if x == "green":
+        return "go"
+    elif x == "yellow":
+        return "slow"
+    else:
+        return "stop"
+
+print(stopLight("red"))
+
+        
